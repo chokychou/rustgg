e3c7663fcf32c5cd30465d93db7f530dec1b54b3
Yi <nobreakfast@foxmail.com>
2021-08-02 19:24:51 -0700

    added credentials to env

---
+++ backend/api/utils/credentials.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
+load_dotenv()
+
+class Credentials:
+    def __init__(self, server=None) -> None:
+        if server=='rustyfire': self.RUSTYFIRE() 
+        if server=='rustygo': self.RUSTYGO()
+    
+    def RUSTYFIRE(self) -> None:
+        self.ADDRESS = os.getenv('RUSTYFIRE_ADDR')
+        self.PASSWORD = os.getenv('RUSTYFIRE_PW')
+        
+    def RUSTYGO(self) -> None:
+        self.ADDRESS = os.getenv('RUSTYGO_ADDR')
+        self.PASSWORD = os.getenv('RUSTYGO_PW')
